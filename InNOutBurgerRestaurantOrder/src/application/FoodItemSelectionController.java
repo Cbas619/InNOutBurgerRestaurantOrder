a190bee42d466de1205c899991936beacaf52093
Cbas <Cbas@10.0.0.167>
2020-11-12 19:15:52 -0800

    added cancel button to second scene.

---
+++ InNOutBurgerRestaurantOrder/src/application/FoodItemSelectionController.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FoodItemSelectionController {
+	
+	/**
+	 * This method is called via the order now button on the startup scene
+	 * When the button is clicked, we start our order and we are
+	 * taken to the next scene where we can select food items
+	 * SOURCE: https://youtu.be/XCgcQTQCfJQ 
+	 * @param event
+	 * @throws IOException
+	 */
+	public void cancelButton(ActionEvent event) throws IOException {
+		Parent parent = FXMLLoader.load(getClass().getResource("InNOutBurgerUI.fxml"));
+		Scene FoodItemSelectionScene = new Scene(parent);
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(FoodItemSelectionScene);
+		window.show();
+	}
+
+}
